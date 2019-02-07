a9734a665786f9c0c41e3f89e8fd17e9bfd6da6b
cheran-senthil <cheran.v.senthil@gmail.com>
2019-02-08 03:27:50 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define all(v) all_of(v.begin(), v.end(), [](bool x) { return x; })
+#define any(v) any_of(v.begin(), v.end(), [](bool x) { return x; })
+#define dict map
+#define float long double
+#define int long long
+#define len(v) (v).size()
+#define list vector
+#define reversed(v) reverse((v).begin(), (v).end())
+#define sorted(v) sort((v).begin(), (v).end())
+#define sum(v) accumulate((v).begin(), (v).end(), 0LL)
+
+#define False false
+#define True true
+#define None nullptr
+
+#define main() signed main()
+
+string input() {
+    string s;
+    getline(cin >> ws, s);
+    return s;
+};
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+main() {
+    ios::sync_with_stdio(False);
+    cin.tie(None);
+
+    return 0;
+}
