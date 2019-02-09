1e1d96475216231c96729806cbb773ec3a760637
cheran-senthil <cheran.v.senthil@gmail.com>
2019-02-09 16:12:51 +0530

    Add _deque struct

--- template.cpp
+++ template.cpp
@@ -40,9 +40,99 @@ template <class T, int N> struct _array : std::array<T, N> {
     }
 };
 
-template <class T, class Allocator = std::allocator<T>>
-struct _vector : std::vector<T, Allocator> {
-    using std::vector<T, Allocator>::vector;
+template <class T, class Alloc = std::allocator<T>>
+struct _deque : std::deque<T, Alloc> {
+    using std::deque<T, Alloc>::deque;
+
+    T &operator[](int i) {
+        if (i < 0) {
+            i += this->size();
+        }
+        return this->at(i);
+    }
+
+    const T &operator[](int i) const {
+        if (i < 0) {
+            i += this->size();
+        }
+        return this->at(i);
+    }
+
+    friend _deque operator+(_deque a, const _deque &b) {
+        a.extend(b);
+        return a;
+    }
+
+    _deque &operator+=(const _deque &a) {
+        extend(a);
+        return *this;
+    }
+
+    void append(const T &x) { this->push_back(x); }
+
+    void appendleft(const T &x) { this->push_front(x); }
+
+    _deque copy() {
+        _deque a(*this);
+        return a;
+    }
+
+    int count(const T &x) { return std::count(this->begin(), this->end(), x); }
+
+    template <class Container> void extend(const Container &iterable) {
+        std::deque<T, Alloc>::insert(this->end(), iterable.begin(),
+                                     iterable.end());
+    }
+
+    template <class Container> void extendleft(const Container &iterable) {
+        std::deque<T, Alloc>::insert(this->begin(), iterable.rbegin(),
+                                     iterable.rend());
+    }
+
+    int index(const T &x, int start = 0, int end = 0) {
+        if (start < 0) {
+            start += this->size();
+        }
+        if (end <= 0) {
+            end += this->size();
+        }
+
+        int i = std::find(this->begin() + start, this->begin() + end, x) -
+                this->begin();
+
+        if (i == end) {
+            throw std::out_of_range("x not in range");
+        }
+
+        return i;
+    }
+
+    void insert(int i, const T &x) {
+        std::deque<T, Alloc>::insert(this->begin() + i, x);
+    }
+
+    T pop() {
+        T x = std::move(this->back());
+        this->pop_back();
+        return x;
+    }
+
+    T popleft() {
+        T x = std::move(this->front());
+        this->pop_front();
+        return x;
+    }
+
+    void remove(const T &x) { this->erase(this->begin() + index(x)); }
+
+    void reverse() { std::reverse(this->begin(), this->end()); }
+
+    void rotate(int n = 1) {}
+};
+
+template <class T, class Alloc = std::allocator<T>>
+struct _vector : std::vector<T, Alloc> {
+    using std::vector<T, Alloc>::vector;
 
     T &operator[](int i) {
         if (i < 0) {
@@ -71,12 +161,12 @@ struct _vector : std::vector<T, Allocator> {
     void append(const T &x) { this->push_back(x); }
 
     template <class Container> void extend(const Container &iterable) {
-        std::vector<T, Allocator>::insert(this->end(), iterable.begin(),
-                                          iterable.end());
+        std::vector<T, Alloc>::insert(this->end(), iterable.begin(),
+                                      iterable.end());
     }
 
     void insert(int i, const T &x) {
-        std::vector<T, Allocator>::insert(this->begin() + i, x);
+        std::vector<T, Alloc>::insert(this->begin() + i, x);
     }
 
     void remove(const T &x) { this->erase(this->begin() + index(x)); }
@@ -156,6 +246,7 @@ string input() {
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 #define array _array
+#define deque _deque
 #define vector _vector
 
 #define complex complex<long double>
