890f93ef3661891ddcb7011ced0af5167449ed89
cheran-senthil <cheran.v.senthil@gmail.com>
2019-04-02 20:04:03 +0530

    Generalized getnum

--- template/template.cpp
+++ template/template.cpp
@@ -8,7 +8,7 @@ template <typename T> T randint(T a, T b) { return uniform_int_distribution<T>(a
 #pragma endregion
 
 #pragma region getnum
-inline int getchar() {
+int getchar() {
 #ifdef _WIN32
     return _getchar_nolock();
 #else
@@ -16,15 +16,34 @@ inline int getchar() {
 #endif
 }
 
-int getnum() {
-    int ret = 0, chr = getchar();
-
+void getnum() {}
+template <class T, class... S> inline void getnum(T &a, S &... b) {
+    int chr = getchar();
     while (isspace(chr))
         chr = getchar();
-    while (isdigit(chr))
-        ret = 10 * ret + chr - '0', chr = getchar();
 
-    return ret;
+    bool positive = true;
+    if (chr == '-')
+        positive = false, chr = getchar();
+
+    if (isdigit(c)) {
+        a = chr - '0';
+        while (isdigit(chr = getchar()))
+            a = a * 10 + chr - '0';
+    } else {
+        a = 0;
+    }
+
+    if (chr == '.') {
+        uint64_t dec_part = 0;
+        int dec_len = 0;
+        while (isdigit(c = getchar()))
+            dec_part = dec_part * 10 + chr - '0', ++dec_len;
+        a += dec_part / pow(10, dec_len);
+    }
+
+    a = positive ? a : -a;
+    getnum(b...);
 }
 
 #pragma endregion
