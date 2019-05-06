af0a9f36380c792265a510868e48593559f51880
cheran-senthil <cheran.v.senthil@gmail.com>
2019-05-06 17:35:54 +0530

    Add simple print

--- template/template.cpp
+++ template/template.cpp
@@ -1,78 +1,56 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
 #include <bits/stdc++.h>
 using namespace std;
 
-int getchar() {
-#ifdef _WIN32
-    return _getchar_nolock();
-#else
-    return getchar_unlocked();
-#endif
+string Sep = " ";
+string End = "\n";
+
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) {
+    os << p.first << Sep << p.second;
+    return os;
 }
 
-void getnum() {}
-template <class T, class... S> inline void getnum(T &a, S &... b) {
-    int chr = getchar();
-    while (isspace(chr))
-        chr = getchar();
-
-    bool positive = true;
-    if (chr == '-')
-        positive = false, chr = getchar();
-
-    if (isdigit(chr)) {
-        a = chr - '0';
-        while (isdigit(chr = getchar()))
-            a = a * 10 + chr - '0';
-    } else {
-        a = 0;
+template <typename T, size_t arr_size, typename = enable_if_t<!is_same<T, char>::value>>
+ostream &operator<<(ostream &os, const T (&arr)[arr_size]) {
+    bool at_start = true;
+    for (const auto &x : arr) {
+        if (!at_start)
+            os << Sep;
+        os << x;
+        at_start = false;
     }
-
-    if (chr == '.') {
-        uint64_t dec_part = 0;
-        int dec_len = 0;
-        while (isdigit(chr = getchar()))
-            dec_part = dec_part * 10 + chr - '0', ++dec_len;
-        a += dec_part / pow(10, dec_len);
-    }
-
-    a = positive ? a : -a;
-    getnum(b...);
+    return os;
 }
 
-string to_string(string s) { return '"' + s + '"'; }
-
-string to_string(const char *s) { return to_string((string)s); }
-
-string to_string(bool b) { return (b ? "true" : "false"); }
-
-template <typename A, typename B> string to_string(pair<A, B> p) {
-    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
-}
-
-template <typename A> string to_string(A v) {
-    bool first = true;
-    string res = "{";
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    bool at_start = true;
     for (const auto &x : v) {
-        if (!first)
-            res += ", ";
-        first = false;
-        res += to_string(x);
+        if (!at_start)
+            os << Sep;
+        os << x;
+        at_start = false;
     }
-    res += "}";
-    return res;
+    return os;
 }
 
-void debug_out() { cerr << endl; }
-
-template <typename Head, typename... Tail> void debug_out(Head H, Tail... T) {
-    cerr << " " << to_string(H);
-    debug_out(T...);
+template <typename T> void print(const T &t) { cout << t << End; }
+template <typename T, typename... ArgTypes> void print(const T &t, ArgTypes const &... args) {
+    cout << t << Sep;
+    print(args...);
 }
 
-#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+void solve() {}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     cout << fixed << setprecision(16);
+    cout << boolalpha;
+
+    solve();
+    return 0;
 }
