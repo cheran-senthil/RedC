db17e66cd097995f8196ac670bc4ff1b7e5585c6
Cheran <cheran.v.senthil@gmail.com>
2019-05-29 07:45:14 +0530

    move print to tools

--- template/template.cpp
+++ template/template.cpp
@@ -1,74 +1,14 @@
-/*
 #pragma GCC optimize("Ofast")
-#pragma GCC target("avx,avx2,fma")
+#pragma GCC target("avx, avx2, fma")
 #pragma GCC optimize("unroll-loops")
-*/
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#pragma region print
-
-string Sep = " ";
-string End = "\n";
-
-template <typename T, typename C>
-vector<T> to_vector(const C &c) { return vector<T>(c.begin(), c.end()); }
-
-template <typename T1, typename T2>
-ostream &operator<<(ostream &os, const pair<T1, T2> &p)
-{
-    os << p.first << Sep << p.second;
-    return os;
+void solve() {
 }
 
-template <typename T, size_t arr_size, typename = enable_if_t<!is_same<T, char>::value>>
-ostream &operator<<(ostream &os, const T (&arr)[arr_size])
-{
-    bool at_start = true;
-    for (const auto &x : arr)
-    {
-        if (!at_start)
-            os << Sep;
-        os << x;
-        at_start = false;
-    }
-    return os;
-}
-
-template <typename T>
-ostream &operator<<(ostream &os, const vector<T> &v)
-{
-    bool at_start = true;
-    for (const auto &x : v)
-    {
-        if (!at_start)
-            os << Sep;
-        os << x;
-        at_start = false;
-    }
-    return os;
-}
-
-template <typename T>
-void print(const T &t) { cout << t << End; }
-template <typename T, typename... ArgTypes>
-void print(const T &t, ArgTypes const &... args)
-{
-    cout << t << Sep;
-    print(args...);
-}
-
-#define debug(...) cout << "[" << #__VA_ARGS__ << "]: ", print(__VA_ARGS__)
-
-#pragma endregion
-
-void solve()
-{
-}
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
---
+++ tools/print.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string Sep = " ";
+string End = "\n";
+
+template <typename T, typename C>
+vector<T> to_vector(const C &c) { return vector<T>(c.begin(), c.end()); }
+
+template <typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2> &p) {
+    os << p.first << Sep << p.second;
+    return os;
+}
+
+template <typename T, size_t arr_size, typename = enable_if_t<!is_same<T, char>::value>>
+ostream &operator<<(ostream &os, const T (&arr)[arr_size]) {
+    bool at_start = true;
+    for (const auto &x : arr) {
+        if (!at_start)
+            os << Sep;
+        os << x;
+        at_start = false;
+    }
+    return os;
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v) {
+    bool at_start = true;
+    for (const auto &x : v) {
+        if (!at_start)
+            os << Sep;
+        os << x;
+        at_start = false;
+    }
+    return os;
+}
+
+template <typename T>
+void print(const T &t) { cout << t << End; }
+template <typename T, typename... ArgTypes>
+void print(const T &t, ArgTypes const &... args) {
+    cout << t << Sep;
+    print(args...);
+}
+
+#define debug(...) cout << "[" << #__VA_ARGS__ << "]: ", print(__VA_ARGS__)
