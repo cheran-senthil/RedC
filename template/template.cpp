9bd7d96f057ac0b9c13e66bba7eda88ba4fb98e6
Cheran <cheran.v.senthil@gmail.com>
2019-06-25 01:24:00 +0530

    bug fix

--- template/template.cpp
+++ template/template.cpp
@@ -1,22 +1,20 @@
-/*
-#pragma GCC optimize("Ofast")
-#pragma GCC target("avx, avx2, fma")
-#pragma GCC optimize("unroll-loops")
-*/
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimize("unroll-loops")
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
 using namespace std;
 
 void solve() {
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-    cout << boolalpha;
-    cout << fixed << setprecision(16);
+    std::cout << std::boolalpha;
+    std::cout << std::fixed << std::setprecision(6);
 
     solve();
-    return 0;
 }
